92a630fb80f7428d8e7375e687e4d85b017e8416
denispan <6231990@gmail.com>
2023-04-10 20:01:42 +0300

    Почтовая рассылка (фронтенд, без бэка)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import './index.scss';
+import { Success } from './components/Success';
+import { Users } from './components/Users';
+
+// Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [onInputValue, setOnInputValue] = useState('');
+  const [invited, setInvited] = useState([]);
+  const [sendInvite, setSendInvite] = useState(false);
+
+  const onClickInvite = (id) => {
+    if (invited.includes(id)) {
+      setInvited(invited.filter(_id => _id !== id));
+    } else {
+      setInvited(prev => [...prev, id]);
+    }
+  }
+
+  const onClickSucces = () => {
+    if (invited.length) setSendInvite(true);
+  }
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then(response => response.json())
+      .then(result => setUsers(result.data))
+      .catch((err) => {
+        console.warn(err);
+        alert(`Ошибка запроса пользователей`);
+      })
+      .finally(() => setIsLoading(false));
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        sendInvite ?
+          <Success setSendInvite={setSendInvite} count={invited.length} /> :
+          <Users
+            onClickSucces={onClickSucces}
+            onInputValue={onInputValue}
+            setOnInputValue={setOnInputValue}
+            items={users}
+            isLoading={isLoading}
+            invited={invited}
+            setInvited={setInvited}
+            onClickInvite={onClickInvite}
+          />
+      }
     </div>
   );
 }
---
+++ src/components/Success.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Success = ({ count, setSendInvite }) => {
+
+  return (
+    <div className="success-block">
+      <img src="/assets/success.svg" alt="Success" />
+      <h3>Успешно!</h3>
+      <p>Всем {count} пользователям отправлено приглашение.</p>
+      <button onClick={() => window.location.reload()} className="send-invite-btn">Назад</button>
+    </div>
+  );
+};
